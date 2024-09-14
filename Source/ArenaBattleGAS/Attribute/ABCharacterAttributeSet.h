8d8c1de8772603ec394cdab97fd7aacc1846cdfe
ideugu <guru@dustinlee.me>
2024-09-15 01:24:52 +0900

    카테고리수정

--- Source/ArenaBattleGAS/Attribute/ABCharacterAttributeSet.h
+++ Source/ArenaBattleGAS/Attribute/ABCharacterAttributeSet.h
@@ -62,7 +62,7 @@ protected:
 	UPROPERTY(BlueprintReadOnly, Category = "Health", Meta = (AllowPrivateAccess = true))
 	FGameplayAttributeData Health;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	UPROPERTY(BlueprintReadOnly, Category = "Health", Meta = (AllowPrivateAccess = true))
 	FGameplayAttributeData MaxHealth;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Attack", Meta = (AllowPrivateAccess = true))
